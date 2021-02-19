85ce0e7475f6766af07624d043c6199406ef8cec
Oscar Nunez <noscar07@gmail.com>
2021-02-18 23:14:44 -0500

    added results on the page

--- code.js
+++ code.js
@@ -10,7 +10,7 @@ async function handleSubmit(event) {
     const searchQuery = inputValue.trim();
     try {
         const results = await searchWikipedia(searchQuery);
-        console.log(results);
+        displayResults(results);
     } catch (err) {
         console.log(err);
         alert('failed to search Wiki')
@@ -20,11 +20,30 @@ async function handleSubmit(event) {
 }
 
 async function searchWikipedia(searchQuery) {
-    const endpoint = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=&srlimit=20&srsearch=SEARCH_QUERY_GOES_HERE'
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`
     const response = await fetch(endpoint);
     if (!response.ok) {
         throw error(response.statusText);
     }
     const json = await response.json();
     return json;
+};
+
+function displayResults(results){
+    // reference to the element
+    const searchResults = document.querySelector('.js-search-results');
+
+    results.query.search.forEach(result => {
+        const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+        searchResults.insertAdjacentHTML (
+            'beforeend',
+            `<div class = 'result-item'>
+             <h3 class='result-title'>
+              <a href='${url}' target= '_blank' rel= 'noopener'>${result.title}</a>
+              </h3>
+              <a href= '${url}' class='result-link' target= '_blank' rel= 'noopener'>${url}</a>
+              <span class='result-snippet'>${result.snippet}</span><br>
+              </div>`
+        );
+    });
 }
